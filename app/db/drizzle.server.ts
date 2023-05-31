a2c1e01008ad9d2c1a5fd71755c9c8a016d5cc28
Aaditya Verma <aaditya220055@gmail.com>
2023-06-01 02:09:16 +0530

    removed migration statement

--- app/db/drizzle.server.ts
+++ app/db/drizzle.server.ts
@@ -1,7 +1,5 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
-import { migrate } from "drizzle-orm/vercel-postgres/migrator";
 import { sql } from "@vercel/postgres";
 import * as schema from "./schema.server";
 
 export const db = drizzle(sql, { schema });
-migrate(db, { migrationsFolder: "./app/db/migrations" });
